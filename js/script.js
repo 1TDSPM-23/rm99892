7d91ca836b9b488041daf50a257c05352905521d
freitaslivia <xliviafreitasx@gmail.com>
2023-05-05 09:22:59 -0300

    declara ver/let/const dif- hosting op/Mat.

---
+++ js/script.js
@@ -0,0 +1,63 @@
+    //     console.log("Olá Mundo")
+
+    //    //COMENTÁRIO DE LINHA
+    //    /* COMENTÁRIO DE BLOCO
+    //    */
+    //     //Declarar uma variável com a palavra reservada var.
+    //     var nr1 = "10";
+    //     var nr2 = "5";
+
+    //     //Vamos imprimir as variáveis com o objeto console.
+    //     console.log(nr1);
+    //     console.log(nr2);
+
+    //     //Realizando as operações básicas com estes valores:
+    //     // + / - / * e /
+
+        
+    //     //REALIZE A CONCATENAÇÃO COM O TEXTO RESULTADO:
+    //     //EX: console.log(RESULTADO nr1 + nr2);
+        
+    //     //ADIÇÃO
+    //     console.log("RESULTADO " + (nr1 + nr2));
+    //     console.log("RESULTADO : " + (parseInt(nr1) + parseInt(nr2)));
+    //     console.log(`RESULTADO ADIÇÃO : ${parseInt(nr1) + parseInt(nr2)}`);
+        
+    //     // SUBTRAÇÃO
+    //     console.log(`RESULTADO SUBTRAÇÃO : ${parseInt(nr1) - parseInt(nr2)}`);
+
+    //     // MULTIPLICAÇÃO
+    //     console.log(`RESULTADO MUTIPLICAÇÃO : ${parseInt(nr1) * parseInt(nr2)}`);
+
+    //     // DIVISÃO
+    //     console.log(`RESULTADO DIVISÃO : ${parseInt(nr1) / parseInt(nr2)}`);
+
+    //     // OBTENHA O RESTO DA DIVISÃO
+    //     console.log(`OBTENDO O RESTO DA DIVISÃO : ${parseInt(nr1) % parseInt(nr2)}`);
+
+
+    //UTILIZANDO OPERADOR TERNÁRIO (? )
+    // var nr1 = "10";
+    // var nr2 = "5";
+    // var resultado;
+
+    // resultado = !(parseInt(nr1) % parseInt(nr2)) ? "PAR" : "IMPAR";
+    // console.log(`O NÚMERO É : ${resultado}`); 
+        
+    //REGRAS DO CONST
+    //1ª Regra = Inicializar na declaração
+    // const nome3 = "Violante";
+    //2ª Regra = Não pode ser alterado durante o fluxo do programa.
+    // nome3 = "Jonathan";
+
+    //DIFERENÇA ENTRE VAR/LET/CONST
+    // var nome1 = "Beatriz";
+    // let nome2 = "Matheus";
+
+    // let nome = "Beatriz";
+
+    // if(nome != ""){
+    //     let nome = "Robson";
+    // }
+    //Qual é o nome que será impresso no log?
+    // console.log(nome);
